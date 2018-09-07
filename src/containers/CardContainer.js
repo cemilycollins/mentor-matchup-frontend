4d68cc760a265fc017477f76546ee5c72222e6f3
Josh <joshua.ross.miller@gmail.com>
2018-09-07 15:24:05 -0400

    newuserform

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -3,15 +3,9 @@ import MentorCard from '../components/MentorCard'
 
 class CardContainer extends React.Component {
   render() {
-    console.log (`woot props `,this.props.users)
     return (
-<<<<<<< HEAD
-      <div>
-        {this.props.users.length > 0 ? this.props.users.map(user => <MentorCard user={user} />): null}
-=======
       <div className="ui cards">
-        {this.props.users.map(user => <MentorCard user={user} />)}
->>>>>>> 797731dfb3e92954bbeafdfe7638604e02a431c6
+        {this.props.users.length > 0 ? this.props.users.map(user => <MentorCard id={user.id}  user={user} />): null}
       </div>
     )
   }
