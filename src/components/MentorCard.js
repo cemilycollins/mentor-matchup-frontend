c0e2352b085f833a34830f249f2298735b6a3592
github username <github email address>
2018-09-07 16:02:59 -0400

    added to CardContainer, MentorCard, and LoginForm

--- src/components/MentorCard.js
+++ src/components/MentorCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class MentorCard extends React.Component {
   render () {
     const {user} = this.props
     return (
-      <div className='card'>
+      <Link to={`/${user.type_of}s/${user.id}`} className='card'>
         <div className='content'>
           <div className='header' id='name'>{user.name}</div>
           <div className='meta'>{user.title}</div>
@@ -13,7 +14,7 @@ class MentorCard extends React.Component {
             <p id='relevant-info'>${user.description}</p>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
