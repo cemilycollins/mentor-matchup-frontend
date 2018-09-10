b858e82abb6df25788379e882caeb2ded329ecf9
github username <github email address>
2018-09-10 09:32:19 -0400

    created mentor detail. Small changes to App and CardContainer

--- src/components/MentorDetail.js
+++ src/components/MentorDetail.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 class MentorDetail extends React.Component {
   render() {
+    console.log(this.props.user)
+    const {user} = this.props
     return (
       <div className="ui segment">
         <div className="ui top attached label">Mentor detail</div>
+        <p><b>Name:</b> {this.props.user.name}</p>
+
 
         <div className='extra content'>
           <div className='ui two buttons'>
-            <div className='ui basic blue button'>Request This Mentor</div>
-            <div className='ui basic button'>Back To All Mentors</div>
+            <div className='ui active basic blue button'>Request This Mentor</div>
+            <div className='ui basic black button'>Back To All Mentors</div>
           </div>
         </div>
       </div>
--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -4,11 +4,16 @@ import MentorDetail from '../components/MentorDetail'
 import { Switch, Route } from 'react-router-dom'
 
 class CardContainer extends React.Component {
+
+  findUser(id) {
+    return this.props.users.find(user => user.id === parseInt(id))
+  }
+
   render() {
     return (
       <Switch>
-        <Route to="/mentors/:user_id" render={ (props) => (<MentorDetail mentorId={props.user_id} />)} />
-        <Route exact to="/mentors" render={() => (
+        <Route path="/mentors/:user_id" render={ (props) => (<MentorDetail user={this.findUser(props.match.params.user_id)}/>)} />
+        <Route exact path="/mentors" render={() => (
           <div className="ui cards">
             {this.props.users.length > 0 ? this.props.users.map(user => <MentorCard user={user} />): null}
           </div>
